867cf8684c595b82fe2e1fc23aefd2c61bd64413
VakhshooriEhsan <vakhshoori.ehsan@gmail.com>
2021-01-27 21:08:01 +0330

    created dataset

--- src/PartA-NonParametricDensityEstimation.py
+++ src/PartA-NonParametricDensityEstimation.py
@@ -1,34 +1,58 @@
 import numpy as np
-from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 
-x, y = np.mgrid[-5.0:15.0:30j, -10.0:10.0:30j]
-# Need an (N, 2) array of (x, y) pairs.
-xy = np.column_stack([x.flat, y.flat])
+def K(u):
+    for i in range(len(u)):
+        if(abs(u[i])>=0.5):
+            return 0
+    return 1
 
-mu = np.array([2.0, 5.0])
-sigma = np.array([[2.0, 0.0], [0.0, 2.0]])
-covariance = np.diag(sigma**2)
-z1 = multivariate_normal.pdf(xy, mean=mu, cov=covariance)/2
-
-mu = np.array([8.0, 1.0])
-sigma = np.array([[3.0, 1.0], [1.0, 3.0]])
-covariance = np.diag(sigma**2)
-z2 = multivariate_normal.pdf(xy, mean=mu, cov=covariance)
-
-mu = np.array([5.0, 3.0])
-sigma = np.array([[2.0, 1.0], [1.0, 2.0]])
-covariance = np.diag(sigma**2)
-z3 = multivariate_normal.pdf(xy, mean=mu, cov=covariance)/2
+def KDE(x, y, h, N, D):
+    p_KDE = np.zeros(N)
+    k=0
+    for i in range(N):
+        k = 0
+        for j in range(N):
+            k += K([(x[i]-x[j])/h, (y[i]-y[j])/h])
+        p_KDE[i] = 1/(N*h1**D)*k
+    return p_KDE
 
 
-# Reshape back to a (30, 30) grid.
-z1 = z1.reshape(x.shape)
-z2 = z2.reshape(x.shape)
-z3 = z3.reshape(x.shape)
+N = 500 # number of examples
+D = 2 # number of dimensions
 
-ax = plt.subplot(111, projection='3d')
-ax.scatter(x, y, z1)
-ax.scatter(x, y, z2)
-ax.scatter(x, y, z3)
+mean = [2, 5]
+cov = [[2, 0], [0, 2]]
+x1, y1 = np.random.multivariate_normal(mean, cov, N).T
+
+mean = [8, 1]
+cov = [[3, 1], [1, 3]]
+x2, y2 = np.random.multivariate_normal(mean, cov, N).T
+
+mean = [5, 3]
+cov = [[2, 1], [1, 2]]
+x3, y3 = np.random.multivariate_normal(mean, cov, N).T
+
+h1 = 0.09
+h2 = 0.3
+h3 = 0.6
+
+p_KDE = KDE(x1, y1, h3, N, D)
+print(p_KDE)
+
+plt.figure(1)
+plt.plot(x1, y1, '.')
+plt.plot(x2, y2, '.')
+plt.plot(x3, y3, '.')
+
+# plt.figure(2)
+# ax = plt.subplot(111, projection='3d')
+# ax.scatter(x1, y1, p_KDE, '.')
+# ax.scatter(x2, y2, p_KDE, '.')
+# ax.scatter(x3, y3, p_KDE, '.')
+
+plt.figure(3)
+plt.hist(p_KDE, bins=25, alpha=0.7)
+# plt.hist(x2, bins=25, alpha=0.7)
+# plt.hist(x3, bins=25, alpha=0.7)
 plt.show()
